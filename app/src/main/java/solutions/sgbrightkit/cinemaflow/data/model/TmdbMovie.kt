92d17f91b9d717dddab252c24959b2b6fbe0295c
alexlukhanin <alex.lukhanin@hotmail.com>
2026-03-19 15:01:05 +0200

    Added TMDB logic for detailscreen

--- app/src/main/java/solutions/sgbrightkit/cinemaflow/data/model/TmdbMovie.kt
+++ app/src/main/java/solutions/sgbrightkit/cinemaflow/data/model/TmdbMovie.kt
@@ -17,3 +17,32 @@ data class TmdbMovie(
     @SerializedName("vote_average") val voteAverage: Double,
     @SerializedName("genre_ids") val genreIds: List<Int>
 )
+
+data class TmdbMovieDetails(
+    val id: Long,
+    val title: String,
+    val overview: String,
+    @SerializedName("poster_path") val posterPath: String?,
+    @SerializedName("backdrop_path") val backdropPath: String?,
+    @SerializedName("release_date") val releaseDate: String?,
+    @SerializedName("vote_average") val voteAverage: Double,
+    val runtime: Int?,
+    val genres: List<TmdbGenre>,
+    val videos: TmdbVideosResponse?
+)
+
+data class TmdbGenre(
+    val id: Int,
+    val name: String
+)
+
+data class TmdbVideosResponse(
+    val results: List<TmdbVideo>
+)
+
+data class TmdbVideo(
+    val key: String,
+    val site: String,
+    val type: String,
+    val name: String
+)
