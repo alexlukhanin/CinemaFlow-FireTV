ded4bb4c6cd7b6eeb5fae8caa670d3ce4664b244
alexlukhanin <alex.lukhanin@hotmail.com>
2026-03-19 22:33:56 +0200

    made theme persistent, preferences manager using SharedPreferences to save darktheme flag

--- app/src/main/java/solutions/sgbrightkit/cinemaflow/MainActivity.kt
+++ app/src/main/java/solutions/sgbrightkit/cinemaflow/MainActivity.kt
@@ -4,17 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
 import solutions.sgbrightkit.cinemaflow.ui.theme.CinemaFlowTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var isDarkTheme by remember { mutableStateOf(false) }
+            val context = LocalContext.current
+            var isDarkTheme by remember {
+                mutableStateOf(PreferencesManager.isDarkTheme(context))
+            }
 
             CinemaFlowTheme(useDarkTheme = isDarkTheme) {
                 CinemaFlowNavGraph(
-                    onThemeToggle = { isDarkTheme = !isDarkTheme }
+                    onThemeToggle = {
+                        isDarkTheme = !isDarkTheme
+                        PreferencesManager.saveDarkTheme(context, isDarkTheme)
+                    }
                 )
             }
         }
